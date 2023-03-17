b89453f25df86984066d40ea213bcf110b5072a9
OguzhanOksuz <oguzhanoksuz0@gmail.com>
2023-03-18 01:12:34 +0300

    quick save

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ooksuz <ooksuz@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 00:43:55 by ooksuz            #+#    #+#             */
-/*   Updated: 2023/03/18 01:10:55 by ooksuz           ###   ########.fr       */
+/*   Updated: 2023/03/18 01:12:25 by ooksuz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,25 @@
 int	error_code(int code)
 {
 	if (code == -1)
-		write(2, "Error invalid extension\n", 26);
+		write(2, "Error invalid extension\n", 25);
 	else if (code == -2)
-		write(2, "Error map is not rectangular\n", 31);
+		write(2, "Error map is not rectangular\n", 30);
 	else if (code == -3)
-		write(2, "Error invalid component\n", 26);
+		write(2, "Error invalid component\n", 25);
 	else if (code == -4)
-		write(2, "Error invalid player count\n", 29);
+		write(2, "Error invalid player count\n", 28);
 	else if (code == -5)
-		write(2, "Error invalid exit count\n", 27);
+		write(2, "Error invalid exit count\n", 26);
 	else if (code == -6)
-		write(2, "Error invalid collectiable count\n", 35);
+		write(2, "Error invalid collectiable count\n", 34);
 	else if (code == -7)
-		write(2, "Error invalid wall design\n", 28);
+		write(2, "Error invalid wall design\n", 27);
 	else if (code == -8)
-		write(2, "Error unreachable component exist\n", 36);
+		write(2, "Error unreachable component exist\n", 35);
 	else if (code == -42)
-		write(2, "Error invalid arguman count\n", 30);
+		write(2, "Error invalid arguman count\n", 29);
 	else if (code == -404)
-		write(2, "Error map file did not found\n", 31);
+		write(2, "Error map file did not found\n", 30);
 	return (-1);
 }
 
