ea90b5af507eb23b4ef71e633b2203876f5d4135
OguzhanOksuz <oguzhanoksuz0@gmail.com>
2023-03-18 23:12:47 +0300

    quick save

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ooksuz <ooksuz@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 00:43:55 by ooksuz            #+#    #+#             */
-/*   Updated: 2023/03/18 01:21:31 by ooksuz           ###   ########.fr       */
+/*   Updated: 2023/03/18 02:25:10 by ooksuz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,7 @@ int	error_code(int code)
 		write(2, "Error map is not rectangular\n", 30);
 	else if (code == -3)
 		write(2, "Error invalid component\n", 25);
-	else if (code == -4)
-		write(2, "Error invalid player count\n", 28);
-	else if (code == -5)
-		write(2, "Error invalid exit count\n", 26);
-	else if (code == -6)
+	else if (code == -6 || code == -5 || code == -4)
 		write(2, "Error invalid collectiable count\n", 34);
 	else if (code == -7)
 		write(2, "Error invalid wall design\n", 27);
@@ -33,14 +29,21 @@ int	error_code(int code)
 	else if (code == -42)
 		write(2, "Error invalid arguman count\n", 29);
 	else if (code == -404)
-		write(2, "Error map file did not found\n", 30);
-	return (-1);
+		write(2, "Error file did not found\n", 26);
+	else if (code == -101010)
+		write(2, "You Dieded\n", 11);
+	else if (code == -1024)
+		write(2, "Exit\n", 5);
+	else
+		write(2, "Error\n", 6);
+	exit(1);
 }
 
 int	main(int ac, char **av)
 {
 	t_map	*ren_map;
 	int		fd;
+	t_game	*game;
 
 	if (ac != 2)
 		return (error_code(-42));
@@ -48,11 +51,15 @@ int	main(int ac, char **av)
 	{
 		fd = open(av[1], O_RDONLY);
 		if (fd < 0)
-			return (error_code(-404));
-		close(fd);
+		{
+			close(fd);
+			error_code(-404);
+		}
 		ren_map = map_init(av[1]);
 		if (ren_map->valid < 0)
-			return (error_code(ren_map->valid));
+			error_code(ren_map->valid);
+		game = init_game(ren_map);
+		lunch_game(game);
 	}
 	system("leaks so_long");
 	return (0);
